f3fcde01128da104b349dbda2f4887c35a300dae
Jerry W <jerrywiltse@gmail.com>
2018-02-03 12:33:55 -0500

    update license capture and recipe vars

--- conanfile.py
+++ conanfile.py
@@ -11,17 +11,19 @@ class DoctestConan(ConanFile):
     url = "https://github.com/bincrafters/conan-doctest"
     description = "C++98/C++11 single header testing framework"
     license = "MIT"
-
+    exports = ["LICENSE.md"]
+    source_subfolder = "source_subfolder"
+    
     def source(self):
         source_url = "https://github.com/onqtam/doctest"
         tools.get("{0}/archive/{1}.tar.gz".format(source_url, self.version))
         extracted_dir = self.name + "-" + self.version
-        os.rename(extracted_dir, "sources")
-        #Rename to "sources" is a convention to simplify later steps
+        os.rename(extracted_dir, self.source_subfolder)
+        #Rename to self.source_subfolder is a convention to simplify later steps
 
     def package(self):
-        self.copy(pattern="LICENSE.txt", dst="licenses", src="sources")
-        self.copy(pattern="*doctest.h*", dst="include", src=os.path.join("sources","doctest"))
+        self.copy(pattern="LICENSE.txt", dst="licenses", src=self.source_subfolder)
+        self.copy(pattern="*doctest.h*", dst="include", src=os.path.join(self.source_subfolder,"doctest"))
 
     def package_id(self):
         self.info.header_only()
